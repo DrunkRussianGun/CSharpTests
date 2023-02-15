11e15df2d6ac6fc521e70bc706e6a0de33d2fdd9
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-02-15 20:17:56 +0400

    Tested clarity of exception message thrown by non-exhaustive switch

---
+++ NetCoreTests/SwitchExpressionShould.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
+
+namespace NetCoreTests;
+
+public class SwitchExpressionShould
+{
+	[Test]
+	public void SpecifyValueInSwitchExpressionException()
+	{
+		Func<int> nonExhaustiveSwitch = () => 42 switch { };
+
+		nonExhaustiveSwitch.Should().Throw<SwitchExpressionException>().WithMessage("*42*");
+	}
+}
