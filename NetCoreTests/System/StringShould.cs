06751d021615b174f8c52988ce4d3eecfd4a7e2f
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-06-07 17:23:38 +0400

    Tested string.Split method on empty input

---
+++ NetCoreTests/System/StringShould.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace NetCoreTests.System;
+
+[Parallelizable(ParallelScope.All)]
+public class StringShould
+{
+	public class OnSplit : StringShould
+	{
+		[Test]
+		public void WhenEmpty_ResultInEmptySubstring()
+		{
+			var substrings = string.Empty.Split('0');
+
+			substrings.Should().BeEquivalentTo(string.Empty);
+		}
+	}
+}
