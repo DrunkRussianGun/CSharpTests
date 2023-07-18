19dfd15f8adaca702497cc8ac413ebc5102711c7
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-07-18 13:30:14 +0400

    Tested == operator with Nullable<Decimal>

---
+++ NetCoreTests/System/DecimalShould.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace NetCoreTests.System;
+
+[Parallelizable(ParallelScope.All)]
+public class DecimalShould
+{
+	[Test]
+	public void WhenNull_NotEqualToZero()
+	{
+		var value = default(decimal?);
+
+		var checkResult = value == 0;
+
+		checkResult.Should().BeFalse();
+	}
+
+	[Test]
+	public void WhenNullableZero_EqualToZero()
+	{
+		var value = (decimal?)0;
+
+		var checkResult = value == 0;
+
+		checkResult.Should().BeTrue();
+	}
+}
