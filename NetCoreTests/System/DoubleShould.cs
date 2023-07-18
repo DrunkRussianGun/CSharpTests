9d72cbe2b4a3402ca0ab562cf646c6698914ece0
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-07-18 13:29:50 +0400

    Tested string representation of double

---
+++ NetCoreTests/System/DoubleShould.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using FluentAssertions;
+
+namespace NetCoreTests.System;
+
+[Parallelizable(ParallelScope.All)]
+public class DoubleShould
+{
+	[TestCase(61.833333333333333333333d, "61.833333333333336")]
+	public void BeRepresentedInaccurately(double value, string expected)
+	{
+		var actual = value.ToString(CultureInfo.InvariantCulture);
+
+		actual.Should().Be(expected);
+	}
+}
