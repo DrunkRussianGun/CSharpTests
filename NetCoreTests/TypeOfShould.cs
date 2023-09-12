e3b2413b6d0b35e1bc8edab43750bd8d750ba99f
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-09-12 18:16:49 +0400

    Tested typeof operator with generics

---
+++ NetCoreTests/TypeOfShould.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace NetCoreTests;
+
+[Parallelizable(ParallelScope.All)]
+public class TypeOfShould
+{
+	[Test]
+	public void GivenGenericType_ReturnNameOfActualType()
+	{
+		var typeName = TypeOf<Guid>().Name;
+
+		typeName.Should().Be("Guid");
+	}
+
+	private static Type TypeOf<T>() => typeof(T);
+}
